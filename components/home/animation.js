9e5b49f5f7a83ea15d63df32387ad58cc2209490
Mickck <mikemwkim@gmail.com>
2022-11-22 02:11:50 +1100

    import useEffect

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
 // Alternatively:
 // import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
