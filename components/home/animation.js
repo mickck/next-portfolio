6e634da7af77ca459c7d90c38de01bbdb3b4a55e
Mickck <mikemwkim@gmail.com>
2022-11-22 04:14:16 +1100

    add img

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,30 +1,8 @@
 import React from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
+import Lottie from "react-lottie-player";
 
-class Animation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.player = React.createRef();
-  }
+import lottieJson from "/public/programming.json";
 
-  doSomething() {
-    this.player.current.play(); // make use of the player and call methods
-  }
-
-  render() {
-    return (
-      <Player
-        onEvent={(event) => {
-          if (event === "load") this.doSomething(); // check event type and do something
-        }}
-        ref={this.player}
-        autoplay={true}
-        loop={true}
-        controls={true}
-        src='https://assets1.lottiefiles.com/private_files/lf30_obidsi0t.json'
-        style={{ height: "100%", width: "100%" }}></Player>
-    );
-  }
+export default function Animation() {
+  return <Lottie loop animationData={lottieJson} play style={{ width: `100%`, height: `100%` }} />;
 }
-
-export default Animation;
