47b040759ee9b0a113c87cc598a53aa8c3b6e1d6
Mickck <mikemwkim@gmail.com>
2022-11-22 04:01:25 +1100

    change public img to url

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,8 +1,30 @@
 import React from "react";
-import Lottie from "react-lottie-player";
+import { Player } from "@lottiefiles/react-lottie-player";
 
-import lottieJson from "/public/animation.json";
+class Animation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.player = React.createRef();
+  }
 
-export default function Animation() {
-  return <Lottie loop animationData={lottieJson} play style={{ width: `100%`, height: `100%` }} />;
+  doSomething() {
+    this.player.current.play(); // make use of the player and call methods
+  }
+
+  render() {
+    return (
+      <Player
+        onEvent={(event) => {
+          if (event === "load") this.doSomething(); // check event type and do something
+        }}
+        ref={this.player}
+        autoplay={true}
+        loop={true}
+        controls={true}
+        src='https://assets1.lottiefiles.com/private_files/lf30_obidsi0t.json'
+        style={{ height: "100%", width: "100%" }}></Player>
+    );
+  }
 }
+
+export default Animation;
