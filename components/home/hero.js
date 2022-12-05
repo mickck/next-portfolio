e18652fcdd20436183356c41661ccb8391f9ef60
Mickck <mikemwkim@gmail.com>
2022-12-06 10:40:57 +1100

    fix typo

--- components/home/hero.js
+++ components/home/hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
           <br className='hidden lg:inline-block' />
         </h1>
         <p className='mb-8 leading-relaxed'>
-          Front End Developer based in Melbourne with experience and passion for responsive websites to deliverer exceptional user experiences. Proficient in JavaScript, TypeScript, HTML/CSS, modern
+          Front End Developer based in Melbourne with experience and passion for responsive websites to deliver exceptional user experiences. Proficient in JavaScript, TypeScript, HTML/CSS, modern
           libraries and frameworks. Passionate about aesthetics and usability.
         </p>
         <div className='flex justify-center'>
